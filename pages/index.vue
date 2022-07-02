f89bb987fb0c26ada6a3edde5e53fede1d6151b6
hhsynalv <hhsynalv@gmail.com>
2022-07-02 20:18:08 +0300

    bug fixed

--- pages/index.vue
+++ pages/index.vue
@@ -63,6 +63,49 @@
             </div>
           </section>
 
+          <section v-if="cyberArticles.length > 0" class="mt-20 w-5/6 lg:w-3/4 md:w-4/5 mx-auto lg:px-4 px-0">
+            <div class="font-montserrat font-medium text-4xl mb-10 text-slate-800 mt-20">
+              <div class="font-montserrat font-medium text-center text-4xl mb-10 text-slate-800 pt-[60px] pb-[60px] underline  decoration-red-400 decoration-4 underline-offset-8">
+                Siber Güvenlik
+              </div>
+            </div>
+
+            <div class="pt-4 grid lg:grid-cols-3 gap-x-8 md:grid-cols-2 sm:grid-cols-1 items-stretch m-3">
+
+              <HomeBlogCard
+                  v-for="article in cyberArticles"
+                  :key="article.title"
+                  :title="article.title"
+                  :img="'/covers/'+article.cover"
+                  :description="article.description"
+                  :date="article.date"
+                  :slug="article.slug"
+                  :tags="article.tags"
+                  :path="article.path"
+              />
+
+              <div class="text-center mt-16 lg:hidden block">
+                <nuxt-link class="relative inline-block group focus:outline-none focus:ring" to="/blog">
+                  <span class="absolute inset-0 transition-transform translate-x-1.5 translate-y-1.5 bg-pink-300 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+                  <span class="relative inline-block px-8 py-3 text-sm font-bold tracking-widest text-black uppercase border-2 border-current group-active:text-opacity-75">
+                    Devamını Oku
+                  </span>
+                </nuxt-link>
+              </div>
+            </div>
+
+            <div class="text-center mt-10 lg:block hidden">
+              <nuxt-link class="relative inline-block group focus:outline-none focus:ring" to="/blog">
+                <span class="absolute inset-0 transition-transform translate-x-1.5 translate-y-1.5 bg-pink-300 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+                <span class="relative inline-block px-8 py-3 text-sm font-bold tracking-widest text-black uppercase border-2 border-current group-active:text-opacity-75">
+                    Daha Fazlası
+                </span>
+              </nuxt-link>
+            </div>
+          </section>
+
           <section v-if="networkArticles.length > 0" class="mt-20 w-5/6 lg:w-3/4 md:w-4/5 mx-auto lg:px-4 px-0">
             <div class="font-montserrat font-medium text-4xl mb-10 text-slate-800 mt-20">
               <div class="font-montserrat font-medium text-center text-4xl mb-10 text-slate-800 pt-[60px] pb-[60px] underline  decoration-red-400 decoration-4 underline-offset-8">
@@ -149,48 +192,7 @@
             </div>
           </section>
 
-          <section v-if="cyberArticles.length > 0" class="mt-20 w-5/6 lg:w-3/4 md:w-4/5 mx-auto lg:px-4 px-0">
-            <div class="font-montserrat font-medium text-4xl mb-10 text-slate-800 mt-20">
-              <div class="font-montserrat font-medium text-center text-4xl mb-10 text-slate-800 pt-[60px] pb-[60px] underline  decoration-red-400 decoration-4 underline-offset-8">
-                Siber Güvenlik
-              </div>
-            </div>
 
-            <div class="pt-4 grid lg:grid-cols-3 gap-x-8 md:grid-cols-2 sm:grid-cols-1 items-stretch m-3">
-
-              <HomeBlogCard
-                  v-for="article in cyberArticles"
-                  :key="article.title"
-                  :title="article.title"
-                  :img="'/covers/'+article.cover"
-                  :description="article.description"
-                  :date="article.date"
-                  :slug="article.slug"
-                  :tags="article.tags"
-                  :path="article.path"
-              />
-
-              <div class="text-center mt-16 lg:hidden block">
-                <nuxt-link class="relative inline-block group focus:outline-none focus:ring" to="/blog">
-                  <span class="absolute inset-0 transition-transform translate-x-1.5 translate-y-1.5 bg-pink-300 group-hover:translate-y-0 group-hover:translate-x-0"></span>
-
-                  <span class="relative inline-block px-8 py-3 text-sm font-bold tracking-widest text-black uppercase border-2 border-current group-active:text-opacity-75">
-                    Devamını Oku
-                  </span>
-                </nuxt-link>
-              </div>
-            </div>
-
-            <div class="text-center mt-10 lg:block hidden">
-              <nuxt-link class="relative inline-block group focus:outline-none focus:ring" to="/blog">
-                <span class="absolute inset-0 transition-transform translate-x-1.5 translate-y-1.5 bg-pink-300 group-hover:translate-y-0 group-hover:translate-x-0"></span>
-
-                <span class="relative inline-block px-8 py-3 text-sm font-bold tracking-widest text-black uppercase border-2 border-current group-active:text-opacity-75">
-                    Daha Fazlası
-                </span>
-              </nuxt-link>
-            </div>
-          </section>
 
         </div>
       </div>
@@ -256,7 +258,7 @@ export default {
           "path",
           "cover"
         ])
-        .where( { categories: { $contains: "network" } } )
+        .where( { categories: { $contains: "Network" } } )
         .limit(3)
         .sortBy("date", "asc")
         .fetch();
@@ -290,7 +292,7 @@ export default {
           "path",
           "cover"
         ])
-        .where( { categories: { $contains: "cyber-security" } } )
+        .where( { categories: { $contains: "Siber Güvenlik" } } )
         .limit(3)
         .sortBy("date", "asc")
         .fetch();
